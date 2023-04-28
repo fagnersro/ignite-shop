7d29a295434cd2721d47722b1f0f0090c799d062
fagnersro <fagner.hg40@gmail.com>
2023-04-28 13:24:25 -0300

    NextJS Fundamentals - Configuring the font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-
 import type { AppProps } from 'next/app'
+import { Roboto } from '@next/font/google'
+
+const roboto = Roboto({
+  weight: ['400', '700'],
+  style: ['normal'],
+  subsets: ['latin']
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <style jsx global>{`html { font-family: ${roboto.style.fontFamily} }`}</style>
+      <Component {...pageProps} />
+    </>
+  ) 
 }
