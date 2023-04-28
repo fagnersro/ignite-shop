b9cc7b628913697f77f6451a23f4c1ed59a5e4ec
fagnersro <fagner.hg40@gmail.com>
2023-04-28 16:16:36 -0300

    Visual structure - Global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app'
 import { Roboto } from '@next/font/google'
+import { globalStyles } from '@/styles/global'
 
 const roboto = Roboto({
   weight: ['400', '700'],
@@ -7,6 +8,7 @@ const roboto = Roboto({
   subsets: ['latin']
 })
 
+globalStyles()
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
