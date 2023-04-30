fcd6f74c8f23b25b95f28ed6772dce21fd98e54d
fagnersro <fagner.hg40@gmail.com>
2023-04-30 18:38:02 -0300

    Product & Checkout - Working with SEO

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { GetStaticProps } from 'next'
 import { stripe } from '@/lib/stripe'
+import Head from 'next/head'
 
 import { HomeContainer, Product } from '@/styles/pages/home'
 import Image from 'next/image'
@@ -29,22 +30,28 @@ const [sliderRef] = useKeenSlider({
 })
 
   return (
-    <HomeContainer ref={sliderRef} className='keen-slider'>
-      {products.map(product => {
-        return (
-          <Link href={`/product/${product.id}`} key={product.id} prefetch={false}>
-          <Product className="keen-slider__slide">
-          <Image  src={product.imageUrl} width={520} height={480} alt=''/>
-  
-          <footer>
-            <strong>{product.name}</strong>
-            <span>{product.price}</span>
-          </footer>
-        </Product>
-        </Link>
-        )
-      })}
-    </HomeContainer>
+    <>
+    <Head>
+      <title>Home | Ignite Shop</title>
+    </Head>
+
+      <HomeContainer ref={sliderRef} className='keen-slider'>
+        {products.map(product => {
+          return (
+            <Link href={`/product/${product.id}`} key={product.id} prefetch={false}>
+            <Product className="keen-slider__slide">
+            <Image  src={product.imageUrl} width={520} height={480} alt=''/>
+    
+            <footer>
+              <strong>{product.name}</strong>
+              <span>{product.price}</span>
+            </footer>
+          </Product>
+          </Link>
+          )
+        })}
+      </HomeContainer>
+    </>
   )
 }
 
