157ae48e2316c7bafd5e19d21a117532655b20b6
fagnersro <fagner.hg40@gmail.com>
2023-04-30 17:06:51 -0300

    Product & Checkout - Successful structure

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,22 @@
+import { ImageContainer, SuccessContainer } from "@/styles/pages/success";
+import Link from "next/link";
+
 export default function Success() {
   return (
-    <h1>Success</h1>
+    <SuccessContainer>
+      <h1>Compra efetuada</h1>
+
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <p>
+        Uhull <strong>Fagner Henrique</strong>, sua <strong>Camiseta Beyound the Limits</strong> já está a caminho de sua casa.
+      </p>
+
+      <Link href="/" >
+        Voltar ao catálogo
+      </Link>
+    </SuccessContainer>
   )
 }
