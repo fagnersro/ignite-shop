2d5a0f4108e5247e8298298085104ce6c87d897f
fagnersro <fagner.hg40@gmail.com>
2023-04-30 11:17:38 -0300

    Product & Checkout - Product page structure 1.0

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,23 @@
+import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/pages/product"
 import { useRouter } from "next/router"
 
 export default function Product() {
   const { query } = useRouter()
   
   return (
-    <h1>Product: {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,98</span>
+
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti, quas! Dignissimos vitae, assumenda facilis tempora quibusdam esse ducimus et repudiandae ullam doloribus, dicta, nam odio quod. Rerum distinctio in facere.</p>
+
+        <button>
+          Comprar agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
