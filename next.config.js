f887b646810450cd68050bc01fd299c20e6ee937
fagnersro <fagner.hg40@gmail.com>
2023-06-05 13:23:18 -0300

    Security config - Headers security

--- next.config.js
+++ next.config.js
@@ -2,6 +2,32 @@
 const nextConfig = {
   reactStrictMode: true,
 
+  async headers() {
+    return [
+      {
+        source: '/src/pages/api',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload'
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          }
+        ]
+      }
+    ]
+  },
+
   images: {
     domains: [
       'files.stripe.com'
